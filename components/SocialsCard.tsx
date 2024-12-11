26ad7d5aea5e232912c665d067852371f1a1c6f5
Gabe Serna <gabriel.serna118@gmail.com>
2024-12-11 09:34:57 -0500

    Add Links to Social Buttons

--- components/SocialsCard.tsx
+++ components/SocialsCard.tsx
@@ -6,42 +6,52 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Download } from "lucide-react";
-import Image from "next/image";
-import Github from "@/public/icons/GitHub.svg";
 import GitHubIcon from "./GitHubIcon";
 import LinkedInIcon from "./LinkedInIcon";
 
 export default function SocialsCard({ name }: { name: string }) {
   let icon: React.ReactNode;
+  let link: string;
 
   switch (name) {
     case "LinkedIn":
       icon = <LinkedInIcon className="text-secondary-foreground" />;
+      link = "https://www.linkedin.com/in/gabe-serna/";
       break;
     case "GitHub":
       icon = <GitHubIcon className="text-secondary-foreground" />;
+      link = "https://github.com/gabe-serna";
       break;
     case "Resume":
       icon = (
         <Download className="stroke-secondary-foreground stroke-[2.5] p-0.5" />
       );
+      link = "https://example.com/resume";
       break;
     default:
       icon = null;
+      link = "#";
   }
 
   return (
     <TooltipProvider delayDuration={100}>
       <Tooltip>
-        <TooltipTrigger className="group focus-visible:outline-none">
-          <Card className="cursor-pointer border-[#493d3f] bg-[#493d3f] hover:bg-muted-foreground group-focus-visible:border-red-400 group-focus-visible:bg-muted-foreground">
-            <CardHeader className="flex-row items-center p-2">
-              {icon}
-              <CardDescription className="sr-only text-sm text-secondary-foreground">
-                {name}
-              </CardDescription>
-            </CardHeader>
-          </Card>
+        <TooltipTrigger asChild className="group focus-visible:outline-none">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group focus-visible:outline-none"
+          >
+            <Card className="cursor-pointer border-[#493d3f] bg-[#493d3f] hover:bg-muted-foreground group-focus-visible:border-red-400 group-focus-visible:bg-muted-foreground">
+              <CardHeader className="flex-row items-center p-2">
+                {icon}
+                <CardDescription className="sr-only text-sm text-secondary-foreground">
+                  {name}
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </a>
         </TooltipTrigger>
         <TooltipContent side="bottom">
           <p>{name}</p>
